5b6a09241792e81f97e10b364cbb3792c2a90ea3
Jeonchanbin <112598893+Jeonchanbin@users.noreply.github.com>
2022-12-07 14:05:14 +0900

    dfsd

--- src/vogue/ListController.java
+++ src/vogue/ListController.java
@@ -3,7 +3,6 @@ package vogue;
 import common.JDBConnector;
 import common.Paging;
 import common.PagingDTO;
-
 /////////////////////////////////////
 // MVC 모델에서 컨트롤러 역할을 하는 클래스//
 /////////////////////////////////////
@@ -18,8 +17,10 @@ public class ListController {
 	// DB연결 클래스 생성
 	JDBConnector jdbc = new JDBConnector();
 
-	// 페이징 클래스 생성 : 생성시 페이징 대상 테이블명을 보낸다
-	Paging pg = new Paging("member");
+	// 페이징 클래스 생성 : 생성시 페이징 대상 테이블명을 보낸다!
+	Paging pg = null; // 기본 선언만 하고 
+	// 아래 setList메서드에서 3가지 값을 보내준다!
+	
 
 	// 페이징 DTO 클래스 생성
 	PagingDTO pgdto = new PagingDTO();
@@ -30,16 +31,36 @@ public class ListController {
 	///////////////////////////////////
 	// 리스트 뷰를 구성하여 리턴하는 메서드 ///
 	///////////////////////////////////
-	public String setList(String pgNum) {
-		// pgNum은 리스트 페이지에서 생성시 파라미터값을 전달해 준다!
+	// pgNum은 리스트 페이지에서 생성시 파라미터값을 전달해 준다!
+	// 검색어 관련 파라미터 pmCol, pmKey 를 전달해 준다!
+	public String setList(String pgNum, String pmCol, String pmKey) {
+		// pgNum - 페이지번호 / pmCol - 검색항목 / pmKey - 검색어
 
+		// 파라미터 전달값 확인!
+		System.out.println("페이지번호:"+pgNum
+				+"\n검색항목:"+pmCol+"\n검색어:"+pmKey);
+		
+		// 페이징 생성자함수 호출!
+		pg = new Paging("member", pmCol, pmKey);
+		
 		// DB레코드결과변수
 		String result = "";
 
 		try {
 
 			// 1. 쿼리문작성 할당
-			String query = "SELECT * FROM `member` ORDER BY `name` ASC LIMIT ?,?";
+			String query = 
+					"SELECT * FROM `member` "+
+					"ORDER BY `name` ASC LIMIT  ?,?";
+			
+			// 1.5. 만약 검색어가 있으면 쿼리 변경!
+			if(pmKey!=null) {
+				query = "SELECT * FROM `member` "
+						+ "WHERE `"+pmCol+"` "
+						+ "LIKE \"%"+pmKey+"%\" "
+						+ "ORDER BY `name` ASC LIMIT  ?,?";
+				System.out.println("널이 아냐!");
+			}
 
 			// 2. 쿼리문 연결 사용준비하기
 			jdbc.pstmt = jdbc.conn.prepareStatement(query);
@@ -110,7 +131,7 @@ public class ListController {
 						jdbc.rs.getString("mid") + "</a></td>" + "   <td>" + 
 						jdbc.rs.getString("name") + "</td>" + "   <td>" + 
 						jdbc.rs.getString("gen") + "</td>" + "   <td>" + 
-						jdbc.rs.getString("email1") + "@" +  
+						jdbc.rs.getString("email1") + "@" + 
 						jdbc.rs.getString("email2") + "</td>" + "   <td>" + 
 						jdbc.rs.getString("auth") + "</td>" + "</tr>";
 				// 일련번호증가
